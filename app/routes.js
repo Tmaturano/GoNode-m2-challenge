16033ebbd6f0add0cd3c1f446967d0bf641cf170
Thiago Maturana <thiagomatu@hotmail.com>
2018-07-31 15:07:06 -0300

    include more configurations for database connection, error middleware, migration for Sessions

---
+++ app/routes.js
@@ -0,0 +1,24 @@
+const express = require('express');
+
+const routes = express.Router();
+
+// Flash Middleware
+routes.use((req, res, next) => {
+  res.locals.flashSuccess = req.flash('success');
+  res.locals.flashError = req.flash('error');
+  next();
+});
+
+routes.use((req, res) => res.render('errors/404'));
+
+// Error Middleware
+routes.use((err, req, res, _next) => {
+  res.status(err.status || 500);
+
+  return res.render('errors/index', {
+    message: err.message,
+    error: process.env.NODE_ENV === 'production' ? {} : err,
+  });
+});
+
+module.exports = routes;
---
+++ config/database.js
@@ -0,0 +1,10 @@
+module.exports = {
+  username: 'sa',
+  password: 'abcd1234!',
+  dialect: 'mssql',
+  dialectOptions: {
+    instanceName: 'sqlexpress',
+  },
+  host: 'localhost',
+  database: 'docfy',
+};
