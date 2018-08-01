74d53cb9440506d0c1eed12d35b4b2ecff3c4e69
Thiago Maturana <thiagomatu@hotmail.com>
2018-08-01 14:24:06 -0300

    included the authentication flow

--- app/routes.js
+++ app/routes.js
@@ -2,12 +2,34 @@ const express = require('express');
 
 const routes = express.Router();
 
+const authMiddleware = require('./middlewares/auth');
+const guestMiddleware = require('./middlewares/guest');
+
+const authController = require('./controllers/authController');
+
 // Flash Middleware
 routes.use((req, res, next) => {
   res.locals.flashSuccess = req.flash('success');
   res.locals.flashError = req.flash('error');
   next();
 });
+
+
+/**
+ * Auth
+ */
+routes.get('/', guestMiddleware, authController.signin);
+routes.get('/signup', guestMiddleware, authController.signup);
+routes.get('/signout', authController.signout);
+routes.post('/register', authController.register);
+routes.post('/authenticate', authController.authenticate);
+
+/**
+ * Dashboard
+ */
+routes.use('/app', authMiddleware);
+// routes.get('/app/dashboard', dashboardController.index);
+
 
 routes.use((req, res) => res.render('errors/404'));
 
