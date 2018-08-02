6a170b3a70b82b72dcb0b4c6adf7744c9fc287ec
Thiago Maturana <thiagomatu@hotmail.com>
2018-08-02 11:52:05 -0300

    included the not empty validation for the project title

--- app/controllers/projectController.js
+++ app/controllers/projectController.js
@@ -1,14 +1,22 @@
+const validator = require('validator');
 const { Project } = require('../models');
 
 module.exports = {
   async store(req, res, next) {
     try {
+      const { title } = req.body.title;
+
+      if (title === undefined || validator.isEmpty(title)) {
+        req.flash('error', 'É necessário informar o nome do novo projeto');
+        return res.redirect('/');
+      }
+
       await Project.create({
         ...req.body,
         UserId: req.session.user.id,
       });
 
-      req.flash('success', `O projeto ${req.body.title} foi criado com sucesso`);
+      req.flash('success', `O projeto ${title} foi criado com sucesso`);
       return res.redirect('/');
     } catch (err) {
       return next(err);
