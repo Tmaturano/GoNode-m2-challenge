e2b421a5ca9ae4fba3e8c889133a1211c24f0640
Thiago Maturana <thiagomatu@hotmail.com>
2018-08-03 15:30:09 -0300

    included the highligh and markdown-it in the section class

--- app/models/section.js
+++ app/models/section.js
@@ -1,7 +1,24 @@
+const hljs = require('highlight.js');
+const md = require('markdown-it')({
+  highlight: (str, lang) => {
+    if (lang && hljs.getLanguage(lang)) {
+      return `<pre class="hljs"><code>${hljs.highlight(lang, str.trim(), true).value}</code></pre>`;
+    }
+
+    return `<pre class="hljs"><code>${md.utils.escape(str.trim())}</code></pre>`;
+  },
+});
+
 module.exports = (sequelize, DataTypes) => {
   const Section = sequelize.define('Section', {
     title: DataTypes.STRING,
     content: DataTypes.STRING,
+  }, {
+    getterMethods: {
+      formattedContent() {
+        return md.render(this.content);
+      },
+    },
   });
 
   Section.associate = (models) => {
